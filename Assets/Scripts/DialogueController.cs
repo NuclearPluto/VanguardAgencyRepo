d8212de6f847c8062b7d08dc9711c25dea055a50
Justin Lee <jlamlee123@gmail.com>
2023-04-06 17:34:12 -0500

    Slight refactoring and logic improvement of Tutorial code

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -19,28 +19,28 @@ public class DialogueController : MonoBehaviour
     void Awake()
     {
         dialogueLines = new List<string>();
-        dialogueLines.Add("Hello Professor. (Click to Advance The Tutorial)");
-        dialogueLines.Add("Looks like a sorry state of things, doesn't it?");
-        dialogueLines.Add("That's what I get for procastinating I guess. (You'd rather not know when I started this.)");
-        dialogueLines.Add("Anyways, here's a brief tutorial to demonstrate what I've implemented.");
-        dialogueLines.Add("You can pan the camera using the WASD keys.");
-        dialogueLines.Add("Great work!");
-        dialogueLines.Add("You can also zoom in and out using the Mouse Scroll Wheel.");
-        dialogueLines.Add("Awesome!");
-        dialogueLines.Add("As you can see, the current map seems to be a little nonuniform.");
-        dialogueLines.Add("This is because the map is procedurally generated! (Though the code is a little messy, I'll refactor it later.)");
-        dialogueLines.Add("Unfortunately the only way to really demonstrate it for now is to restart the exe.");
-        dialogueLines.Add("There are also currently different possible map sizes, small medium and large.");
-        dialogueLines.Add("Currently the only way to demonstrate those is to change the serialized variable Stage Size attached to the Director within the editor itself.");
-        dialogueLines.Add("You can also drag and select units by holding the Left Mouse Button and releasing.");
-        dialogueLines.Add("Try selecting me!");
-        dialogueLines.Add("Now that I'm selected, you can also send me to go somewhere using the Right Mouse Button.");
-        dialogueLines.Add("Try it!");
-        dialogueLines.Add("Well. Ideally, I won't be floating to places and phasing through walls.");
-        dialogueLines.Add("That's pretty much it for now.");
-        dialogueLines.Add("Now that I've finally started, that initial dreadful barrier is gone and I should be working on this project a lot more now.");
-        dialogueLines.Add("Hopefully next time you see me, I'll be a lot more impressive.");
-        dialogueLines.Add("Goodbye professor.");
+        /*0*/dialogueLines.Add("Hello Professor. (Click to Advance The Tutorial)");
+        /*1*/dialogueLines.Add("Looks like a sorry state of things, doesn't it?");
+        /*2*/dialogueLines.Add("That's what I get for procastinating I guess. (You'd rather not know when I started this.)");
+        /*3*/dialogueLines.Add("Anyways, here's a brief tutorial to demonstrate what I've implemented.");
+        /*5*/dialogueLines.Add("You can pan the camera using the WASD keys.");
+        /*6*/dialogueLines.Add("Great work!");
+        /*7*/dialogueLines.Add("You can also zoom in and out using the Mouse Scroll Wheel.");
+        /*8*/dialogueLines.Add("Awesome!");
+        /*9*/dialogueLines.Add("As you can see, the current map seems to be a little nonuniform.");
+        /*10*/dialogueLines.Add("This is because the map is procedurally generated! (Though the code is a little messy, I'll refactor it later.)");
+        /*11*/dialogueLines.Add("Unfortunately the only way to really demonstrate it for now is to restart the exe.");
+        /*12*/dialogueLines.Add("There are also currently different possible map sizes, small medium and large.");
+        /*13*/dialogueLines.Add("Currently the only way to demonstrate those is to change the serialized variable Stage Size attached to the Director within the editor itself.");
+        /*14*/dialogueLines.Add("You can also drag and select units by holding the Left Mouse Button and releasing.");
+        /*15*/dialogueLines.Add("Try selecting me!");
+        /*16*/dialogueLines.Add("Now that I'm selected, you can also send me to go somewhere using the Right Mouse Button.");
+        /*17*/dialogueLines.Add("Try it!");
+        /*18*/dialogueLines.Add("Well. Ideally, I won't be floating to places and phasing through walls.");
+        /*19*/dialogueLines.Add("That's pretty much it for now.");
+        /*20*/dialogueLines.Add("Now that I've finally started, that initial dreadful barrier is gone and I should be working on this project a lot more now.");
+        /*21*/dialogueLines.Add("Hopefully next time you see me, I'll be a lot more impressive.");
+        /*22*/dialogueLines.Add("Goodbye professor.");
     }
 
     void Start()
@@ -59,18 +59,21 @@ public class DialogueController : MonoBehaviour
             if (currentCoroutine != null) {
                 StopCoroutine(currentCoroutine);
             }
-            if (currentLine == 5)
+            if (currentLine == 4)
             {
+                ShowNextDialogueLine();
                 currentCoroutine = StartCoroutine(ListenForWASD());
                 StartCoroutine(WaitForAnswer());
             }
-            else if (currentLine == 7)
+            else if (currentLine == 6)
             {
+                ShowNextDialogueLine();
                 currentCoroutine = StartCoroutine(ListenForScroll());
                 StartCoroutine(WaitForAnswer());
             }
-            else if (currentLine == 15) 
+            else if (currentLine == 14) 
             {
+                ShowNextDialogueLine();
                 //Debug.Log("fiosadjfioasdj");
                 GameObject tempManager = GameObject.FindWithTag("SelectionManager");
                 //Debug.Log(tempManager);
@@ -78,8 +81,9 @@ public class DialogueController : MonoBehaviour
                 currentCoroutine = StartCoroutine(ListenForPlayerSelect(tempManager.GetComponent<SelectionManager>()));
                 StartCoroutine(WaitForAnswer());
             }
-            else if (currentLine == 17) 
+            else if (currentLine == 16) 
             {
+                ShowNextDialogueLine();
                 GameObject tempManager = GameObject.FindWithTag("SelectionManager");
                 currentCoroutine = StartCoroutine(ListenForPlayerMovement(tempManager.GetComponent<SelectionManager>()));
                 StartCoroutine(WaitForAnswer());
@@ -112,52 +116,24 @@ public class DialogueController : MonoBehaviour
 
     private IEnumerator ListenForWASD()
     {
-        while (!answer)
-        {
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
-            {
-                Debug.Log("Yup");
-                answer = true;
-                yield break;
-            }
-            yield return null;
-        }
+        yield return new WaitUntil(() => (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)));
+        answer = true;
     }
 
     private IEnumerator ListenForScroll()
     {
-        while (!answer)
-        {
-            if (Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")) > 0f)
-            {
-                answer = true;
-                yield break;
-            }
-            yield return null;
-        }
+        yield return new WaitUntil(() => (Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")) > 0f));
+        answer = true;
     }
+
     private IEnumerator ListenForPlayerSelect(SelectionManager tempManager)
     {
-        while (!answer)
-        {
-            if (tempManager.hasSelectedUnits())
-            {
-                answer = true;
-                yield break;
-            }
-            yield return null;
-        }
+        yield return new WaitUntil(() => (tempManager.hasSelectedUnits()));
+        answer = true;
     }
     private IEnumerator ListenForPlayerMovement(SelectionManager tempManager)
     {
-        while (!answer)
-        {
-            if (tempManager.hasSelectedUnits() && Input.GetMouseButtonDown(1))
-            {
-                answer = true;
-                yield break;
-            }
-            yield return null;
-        }
+        yield return new WaitUntil(() => (tempManager.hasSelectedUnits() && Input.GetMouseButtonDown(1)));
+        answer = true;
     }
 }
